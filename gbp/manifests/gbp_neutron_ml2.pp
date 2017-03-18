2b838603a3b8bc9ae41f653e79d1662b8ae752d3
Iftikhar Rathore <irathore@cisco.com>
2017-03-18 16:56:08 -0700

    Added extension_drivers

--- gbp/manifests/gbp_neutron_ml2.pp
+++ gbp/manifests/gbp_neutron_ml2.pp
@@ -1,16 +1,8 @@
 class gbp::gbp_neutron_ml2 () {
 
+    $inifile = { 'path' => '/etc/neutron/plugins/ml2/ml2_conf.ini' }
     if hiera('CONFIG_APIC_PLUGIN_MODE') == 'gbp' {
        $mech = 'apic_gbp'
-    }
-    elsif hiera('CONFIG_APIC_PLUGIN_MODE') == 'unified' {
-       $mech = 'apic_aim'
-    }
-    else {
-       $mech = 'cisco_apic_ml2'
-    }
-
-    $inifile = { 'path' => '/etc/neutron/plugins/ml2/ml2_conf.ini' }
     $params = {
       'ml2' => {
         'type_drivers'  => 'opflex,local,flat,vlan,gre,vxlan',
@@ -18,6 +10,32 @@ class gbp::gbp_neutron_ml2 () {
         'mechanism_drivers'  => $mech,
        },
      }
+
+    }
+    elsif hiera('CONFIG_APIC_PLUGIN_MODE') == 'unified' {
+       $mech = 'apic_aim'
+    $params = {
+      'ml2' => {
+        'type_drivers'  => 'opflex,local,flat,vlan,gre,vxlan',
+        'tenant_network_types'  => 'opflex',
+        'mechanism_drivers'  => $mech,
+        'extension_drivers'  => $mech,
+       },
+     }
+
+    }
+    else {
+       $mech = 'cisco_apic_ml2'
+    $params = {
+      'ml2' => {
+        'type_drivers'  => 'opflex,local,flat,vlan,gre,vxlan',
+        'tenant_network_types'  => 'opflex',
+        'mechanism_drivers'  => $mech,
+       },
+     }
+
+    }
+
 create_ini_settings($params, $inifile)
 
 }
