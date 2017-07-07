8967a0c5cfcf2d6a8b37a7a4dab0fdb95088984e
Iftikhar Rathore <irathore@cisco.com>
2017-07-06 17:09:42 -0700

    Fixed broken support for non-unified

--- gbp/manifests/init.pp
+++ gbp/manifests/init.pp
@@ -34,7 +34,7 @@ class gbp() {
 
    if hiera('CONFIG_APIC_ROLE') == 'controller' {
        
-       if hiera('CONFIG_APIC_PLUGIN_MODE') != 'unified' {
+       if hiera('CONFIG_APIC_PLUGIN_MODE') != 'ml2' {
 
            if !defined(Package['openstack-neutron-gbp']) {
               package {'openstack-neutron-gbp':
@@ -66,7 +66,7 @@ class gbp() {
               }
            }
        }
-       else {
+       if hiera('CONFIG_APIC_PLUGIN_MODE') == 'unified' { 
 
            if !defined(Package['aci-integration-module']) {
               package {'aci-integration-module':
@@ -74,37 +74,7 @@ class gbp() {
               }
            }
 
-           if !defined(Package['openstack-neutron-gbp']) {
-              package {'openstack-neutron-gbp':
-                 ensure => installed,
-              }
-           }
-
-           if !defined(Package['python-gbpclient']) {
-              package {'python-gbpclient':
-                 ensure => installed,
-              }
-           }
-
-           if !defined(Package['openstack-heat-gbp']) {
-              package {'openstack-heat-gbp':
-                 ensure => installed,
-              }
-           }
-
-           if !defined(Package['openstack-dashboard-gbp']) {
-              package {'openstack-dashboard-gbp':
-                 ensure => installed,
-              }
-           }
-
-           if !defined(Package['python-django-horizon-gbp']) {
-              package {'python-django-horizon-gbp':
-                 ensure => installed,
-              }
-           }
-       }
-
+        }
     }
     else {
     
